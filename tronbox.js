5febb458d5d2457b5c8f3b961a9aabb3895d6333
Srinjoy Chakravarty <chaxz93@gmail.com>
2019-05-05 18:28:46 +0530

    removed some commented out lines

--- tronbox.js
+++ tronbox.js
@@ -1,7 +1,6 @@
 module.exports = {
   networks: {
     development: {
-// For trontools/quickstart docker image
       privateKey: 'ee3572e2f996a296758788936b4c936b3cdf8c952ab1ce74134965f42c7cbfa1',
       consume_user_resource_percent: 30,
       fee_limit: 100000000,
@@ -20,8 +19,6 @@ module.exports = {
       network_id: "*"
     },
     mainnet: {
-// Don't put your private key here, pass it using an env variable, like:
-// PK=da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0 tronbox migrate --network mainnet
       privateKey: process.env.PK,
       consume_user_resource_percent: 30,
       fee_limit: 100000000,
